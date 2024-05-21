378648b59626c0b6b3a63f269842aedf00deafc7
Sugiannoor <akhmadsugiannoor9@gmail.com>
2024-05-21 09:17:31 +0800

    Login

--- src/layout/AdminLayout.tsx
+++ src/layout/AdminLayout.tsx
@@ -2,23 +2,31 @@ import { Header } from "@/components/navbar/HeaderAdmin";
 import { useAuth } from "@/hooks/authHook";
 import { Button } from "@mantine/core";
 import { IconArrowLeft, IconHome } from "@tabler/icons-react";
-import { Outlet, useNavigate } from "react-router-dom";
+import { Navigate, Outlet, useNavigate } from "react-router-dom";
 
 export const AdminLayout = () => {
   const navigate = useNavigate();
   const { creds } = useAuth();
-  // if (creds?.role === "Customer") return <Navigate to="/" replace />;
-  // if (!creds) return <Navigate to="/" replace />;
+  if (creds?.role === "Customer") return <Navigate to="/" replace />;
+  if (!creds) return <Navigate to="/" replace />;
   return (
     <div className="mx-auto max-w-[90%]">
       <div className="mb-8">
         <Header />
       </div>
       <section className="flex justify-end gap-4">
-        <Button color="red" leftSection={<IconArrowLeft />} onClick={() => navigate(-1)}>
+        <Button
+          color="red"
+          leftSection={<IconArrowLeft />}
+          onClick={() => navigate(-1)}
+        >
           Kembali
         </Button>
-        <Button color="blue" leftSection={<IconHome />} onClick={() => navigate("/admin/dashboard")}>
+        <Button
+          color="blue"
+          leftSection={<IconHome />}
+          onClick={() => navigate("/admin/dashboard")}
+        >
           Dashboard
         </Button>
       </section>
